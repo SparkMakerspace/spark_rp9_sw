c13165d1b18d57d6120f175f538f28fbe3c63a76
John Scimone <john@scimone.net>
2022-08-02 00:21:43 -0400

    cleaned up dead and commented code

--- test_code/src/main.cpp
+++ test_code/src/main.cpp
@@ -1,9 +1,6 @@
 #include <Arduino.h>
 #include <xiaorp2040.h>
-#include <Adafruit_TinyUSB.h>
 #include <Adafruit_NeoPixel.h>
-
-void hid_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize);
 
 //------------- Neopixel -------------//
 #ifdef PIN_NEOPIXEL
@@ -15,24 +12,10 @@ void hid_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEOPIXEL_NUM, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 #endif
 
-uint8_t const desc_hid_report[] =
-    {
-        TUD_HID_REPORT_DESC_KEYBOARD()};
-
 uint8_t cols[] = {D8, D9, D10};
 uint8_t rows[] = {D1, D2, D3};
 
-// Key definitions!
-uint8_t hidcode[] = {HID_KEY_7, HID_KEY_8, HID_KEY_9,
-                     HID_KEY_4, HID_KEY_5, HID_KEY_6,
-                     HID_KEY_1, HID_KEY_2, HID_KEY_3};
-
-// Init Keyboard
-Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_KEYBOARD, 2, false);
-
-bool activeState = false;
-
-uint8_t led_blink_divider;
+uint8_t led_blink_counter;
 uint8_t led_index = 0;
 
 uint8_t builtin_leds[] = {LED_BUILTIN_R, LED_BUILTIN_G, LED_BUILTIN_B};
@@ -44,14 +27,10 @@ uint8_t builtin_leds[] = {LED_BUILTIN_R, LED_BUILTIN_G, LED_BUILTIN_B};
 ////////////////////////////////////////////////////////////////////////////////
 void setup()
 {
-  led_blink_divider = 0;
+  led_blink_counter = 0;
 #if defined(PIN_NEOPIXEL_POWER)
   pinMode(PIN_NEOPIXEL_POWER, OUTPUT);
   digitalWrite(PIN_NEOPIXEL_POWER, 1);
-#endif
-#if defined(ARDUINO_ARCH_MBED) && defined(ARDUINO_ARCH_RP2040)
-  // Manual begin() is required on core without built-in support for TinyUSB such as mbed rp2040
-  TinyUSB_Device_Init(0);
 #endif
 
   // Setup LEDs
@@ -70,27 +49,13 @@ void setup()
 
   // Neopixel
   pixels.begin();
-  pixels.setBrightness(50); // out of 255
-
-  usb_hid.setBootProtocol(HID_ITF_PROTOCOL_KEYBOARD);
-  usb_hid.setPollInterval(2);
-  usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
-  usb_hid.setStringDescriptor("TinyUSB Keyboard");
-
-  // Set up output report (on control endpoint) for Capslock indicator
-  // usb_hid.setReportCallback(NULL, hid_report_callback);
-
-  usb_hid.begin();
+  pixels.setBrightness(20); // out of 255
 
   for (uint8_t i = 0; i < 3; i++)
   {
     pinMode(cols[i], OUTPUT);
     pinMode(rows[i], INPUT_PULLDOWN);
   }
-
-  // wait until device mounted
-  while (!TinyUSBDevice.mounted())
-    delay(1);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -104,119 +69,85 @@ void loop()
   // poll gpio once each 2 ms
   delay(2);
 
-  led_blink_divider += 1U;
-  led_blink_divider = (led_blink_divider < 128)? led_blink_divider : 0;
-  if (led_blink_divider == 0)
+  // Count loops
+  led_blink_counter += 1U;
+  // reset the count at 32
+  led_blink_counter = (led_blink_counter < 32)? led_blink_counter : 0;
+  // when the counter is 0, change color
+  if (led_blink_counter == 0)
   {
+    // loop over all builtin LEDs turning them all off - this could be much more efficient
     for (uint8_t i = 0; i < sizeof(builtin_leds); i++)
     {
       digitalWrite(builtin_leds[i], 1); // turn off all builtin LEDS
     }
+    // the value of led_index (0, 1, or 2) tells us which buildin LED to turn on
     digitalWrite(builtin_leds[led_index], 0); // turn on the one we want
+    // increment the led index for next time
     led_index += 1U;
+    // wrap the led index back to 0 after 2 since there are only three of them
     led_index = (led_index < 3)? led_index : 0;
   }
-
-  // used to avoid send multiple consecutive zero report for keyboard
-  // static bool keyPressedPreviously = false;
-  uint8_t count = 0;
-  // uint8_t keycode[9] = {0};
 
   for (uint8_t i = 0; i < 3; i++)
   {
     // go through each column
     for (uint8_t h = 0; h < 3; h++)
     {
-      // set all non-current columns to 0
+      // set the active column to 1, otherwise 0.
       if (h == i)
       {
         digitalWrite(cols[i], 1);
       }
       else
       {
-        // set current column to 1
         digitalWrite(cols[h], 0);
       }
+      delay(2);
     }
-
-    delay(1); // prevents accidental reads while prev column was "on"
 
     for (uint8_t j = 0; j < 3; j++)
     {
       // check each row
       if (digitalRead(rows[j]))
       {
-        // keycode[count++] = hidcode[(j * 3) + i]; // put the key press in the buffer
-        if (j == 0)
+        if (j == 0) // top row
         {
-          if (i == 0)
+          if (i == 0) // left column 
             digitalWrite(LED0, 0); // turn on
           else
             digitalWrite(LED0, 1); // turn off
-          if (i == 1)
+          if (i == 1) // middle column
             digitalWrite(LED1, 0); // turn on
           else
             digitalWrite(LED1, 1); // turn off
-          if (i == 2)
+          if (i == 2) // right column
             digitalWrite(LED2, 0); // turn on
           else
             digitalWrite(LED2, 1); // turn off
         }
-        else if (j == 1)
+        else if (j == 1) // middle row
         {
-          if (i == 0)
+          if (i == 0) // left column
             pixels.fill(0xff0000);
-          if (i == 1)
+          if (i == 1) // middle column
             pixels.fill(0x00ff00);
-          if (i == 2)
+          if (i == 2) // right column
             pixels.fill(0x0000ff);
           pixels.show();
         }
-        else if (j == 2)
+        else if (j == 2) // bottom row
         {
-          if (i == 0)
+          if (i == 0) // left column
             pixels.fill(0xffff00);
-          if (i == 1)
+          if (i == 1) // middle column
             pixels.fill(0x00ffff);
-          if (i == 2)
+          if (i == 2) // right column
             pixels.fill(0xff00ff);
           pixels.show();
         }
       }
-      if (count == 9)
-        break;
     }
   }
 
-  if (TinyUSBDevice.suspended() && count)
-  {
-    // Wake up host if we are in suspend mode
-    // and REMOTE_WAKEUP feature is enabled by host
-    TinyUSBDevice.remoteWakeup();
-  }
-
-  // skip if hid is not ready e.g still transferring previous report
-  if (!usb_hid.ready())
-    return;
-
-  // if (count)
-  // {
-  //   // Send report if there is key pressed
-  //   uint8_t const report_id = 0;
-  //   uint8_t const modifier = 0;
-
-  //   keyPressedPreviously = true;
-  //   usb_hid.keyboardReport(report_id, modifier, keycode);
-  // }
-  // else
-  // {
-  //   // Send All-zero report to indicate there is no keys pressed
-  //   // Most of the time, it is, though we don't need to send zero report
-  //   // every loop(), only a key is pressed in previous loop()
-  //   if (keyPressedPreviously)
-  //   {
-  //     keyPressedPreviously = false;
-  //     usb_hid.keyboardRelease(0);
-  //   }
-  // }
 }
